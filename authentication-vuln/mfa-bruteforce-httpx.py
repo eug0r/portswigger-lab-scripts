62a931290d92ecd8e3321c570862c8a18ab2811c
eugor <57770278+eug0r@users.noreply.github.com>
2025-03-26 23:22:14 +0300

    Update mfa-bruteforce-httpx.py

--- authentication-vuln/mfa-bruteforce-httpx.py
+++ authentication-vuln/mfa-bruteforce-httpx.py
@@ -9,7 +9,7 @@ login = f"{url}/login"
 login2 = f"{url}/login2"
 semaphore = asyncio.Semaphore(24)
 
-# Async CSRF fetch using regex for speed
+# CSRF fetch
 def csrf_fetch(response_text):
     match = re.search(r'name="csrf" value="(.+?)"', response_text)
     return match.group(1) if match else None
